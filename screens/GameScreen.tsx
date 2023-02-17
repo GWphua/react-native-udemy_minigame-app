b17cbcd5b68f611020d70cd23f1f8987195ad55a
Guan Wei <phuaguanwei99@gmail.com>
2023-02-17 12:27:33 +0800

    Basic layout of Game Screen

--- screens/GameScreen.tsx
+++ screens/GameScreen.tsx
@@ -1,8 +1,34 @@
-import React, { FC } from 'react';
-import { Text } from 'react-native';
+import React, { FC } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 const GameScreen: FC = () => {
-  return <Text>Game Screen!</Text>
-}
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.title}>Opponent's Guess</Text>
+
+      <View>
+        <Text>Higher or lower?</Text>
+      </View>
+      {/* <View>LOG ROUNDS</View> */}
+    </View>
+  );
+};
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 12,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#ddb52f",
+    borderWidth: 2,
+    borderColor: "#ddb52f",
+    padding: 12,
+  },
+});
