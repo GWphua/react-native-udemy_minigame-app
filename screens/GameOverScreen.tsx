63b3e49f024584c9c377e3b8fa5cae494c4357a5
Guan Wei <phuaguanwei99@gmail.com>
2023-02-20 14:40:43 +0800

    Adding a oversized image using StyleSheet.

--- screens/GameOverScreen.tsx
+++ screens/GameOverScreen.tsx
@@ -1,6 +1,8 @@
 import React, { FC } from "react";
-import { StyleSheet, TextInput, View } from "react-native";
+import { Image, StyleSheet, Text, TextInput, View } from "react-native";
 import PrimaryButton from "../components/ui/PrimaryButton";
+import Title from "../components/ui/Title";
+import Colors from "../constants/colors";
 
 interface IGameOverScreen {
   onRestart: () => void;
@@ -8,7 +10,16 @@ interface IGameOverScreen {
 
 const GameOverScreen: FC<IGameOverScreen> = ({ onRestart }) => {
   return (
-    <View>
+    <View style={styles.rootContainer}>
+      <Title>Game Over</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/success.png")}
+        />
+      </View>
+      <Text>Your phone needed X rounds to guess the number Y.</Text>
+
       <PrimaryButton onPress={onRestart}>Restart</PrimaryButton>
     </View>
   );
@@ -16,4 +27,24 @@ const GameOverScreen: FC<IGameOverScreen> = ({ onRestart }) => {
 
 export default GameOverScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imageContainer: {
+    borderRadius: 150,
+    width: 300,
+    height: 300,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    overflow: "hidden",
+    margin: 36,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
